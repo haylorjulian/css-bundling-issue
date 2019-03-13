9fb5852a12a8905566df9f96dc3d01145fe027d0
Julian Haylor <haylorjulian@gmail.com>
2019-03-13 16:51:58 +0000

    Updates

---
+++ precache-manifest.49313324475c7af5961cfd47ea40cbaf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9402584370bd8d1471d7",
+    "url": "/myapp/static/js/runtime~main.990481e1.js"
+  },
+  {
+    "revision": "f7b2f863d0562d4b25dd",
+    "url": "/myapp/static/js/main.757a0c19.chunk.js"
+  },
+  {
+    "revision": "b354ab299cefffce9aa9",
+    "url": "/myapp/static/js/home.241ddad8.chunk.js"
+  },
+  {
+    "revision": "39f3185626cf11d05642",
+    "url": "/myapp/static/js/about.9aa38fb0.chunk.js"
+  },
+  {
+    "revision": "86ece4465f3ed348fe0a",
+    "url": "/myapp/static/js/4.93eeaa20.chunk.js"
+  },
+  {
+    "revision": "f7b2f863d0562d4b25dd",
+    "url": "/myapp/static/css/main.fcf4768f.chunk.css"
+  },
+  {
+    "revision": "b354ab299cefffce9aa9",
+    "url": "/myapp/static/css/home.ee7cdf22.chunk.css"
+  },
+  {
+    "revision": "39f3185626cf11d05642",
+    "url": "/myapp/static/css/about.814921c1.chunk.css"
+  },
+  {
+    "revision": "8387327da7811be6930d5b9cacbdb4d9",
+    "url": "/myapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/css-modules-issue/precache-manifest.07aecf7a40c416447065b8df6b30659b.js"
+  "/myapp/precache-manifest.49313324475c7af5961cfd47ea40cbaf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/css-modules-issue/index.html", {
+workbox.routing.registerNavigationRoute("/myapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
